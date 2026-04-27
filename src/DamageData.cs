1dc35bc9b81ca3a1dc642a2a5e67442608833a53
Enriko 'HybridMind' Todorov <hybridmind1337@gmail.com>
2026-04-27 23:40:30 +0100

    Update DamageData.cs

--- src/DamageData.cs
+++ src/DamageData.cs
@@ -5,14 +5,13 @@ public class DamageEntry
     public int DamageHP { get; set; } = 0;
     public int DamageArmor { get; set; } = 0;
     public int Hits { get; set; } = 0;
-    public List<string> Hitgroups { get; set; } = new();
+    public bool Headshot { get; set; } = false;
     public bool IsFriendlyFire { get; set; } = false;
 }
 
 public class RecentDamage
 {
     public int TotalDamage { get; set; } = 0;
-    public int ArmorDamage { get; set; } = 0;
     public string LastHitgroup { get; set; } = "";
     public DateTime LastDamageTime { get; set; } = DateTime.MinValue;
 }
@@ -23,17 +22,14 @@ public class PlayerData
     public int VictimKillerSlot { get; set; } = -1;
     public string? CenterMessage { get; set; } = null;
     public CounterStrikeSharp.API.Modules.Timers.Timer? CenterTimer { get; set; } = null;
-
     public Dictionary<int, DamageEntry> GivenDamage { get; } = new();
     public Dictionary<int, DamageEntry> TakenDamage { get; } = new();
-
     public Dictionary<int, RecentDamage> RecentDamages { get; } = new();
 }
 
 public class DamageTracker
 {
     private readonly Dictionary<int, PlayerData> _data = new();
-
     private readonly Dictionary<int, string> _nameCache = new();
 
     public void CacheName(int slot, string name)
@@ -59,7 +55,7 @@ public class DamageTracker
 
     public IEnumerable<KeyValuePair<int, PlayerData>> All() => _data;
 
-    public void RecordDamage(int attackerSlot, int victimSlot, int hp, int armor, string hitgroup, bool friendlyFire)
+    public void RecordDamage(int attackerSlot, int victimSlot, int hp, int armor, int hitgroup, bool friendlyFire)
     {
         if (attackerSlot == victimSlot) return;
 
@@ -72,8 +68,7 @@ public class DamageTracker
         given.DamageHP    += hp;
         given.DamageArmor += armor;
         given.Hits++;
-        if (!string.IsNullOrEmpty(hitgroup))
-            given.Hitgroups.Add(hitgroup);
+        if (hitgroup == 1) given.Headshot = true;
 
         var victimData = GetOrCreate(victimSlot);
         if (!victimData.TakenDamage.TryGetValue(attackerSlot, out var taken))
@@ -84,8 +79,7 @@ public class DamageTracker
         taken.DamageHP    += hp;
         taken.DamageArmor += armor;
         taken.Hits++;
-        if (!string.IsNullOrEmpty(hitgroup))
-            taken.Hitgroups.Add(hitgroup);
+        if (hitgroup == 1) taken.Headshot = true;
 
         if (!attackerData.RecentDamages.TryGetValue(victimSlot, out var recent))
         {
@@ -96,13 +90,9 @@ public class DamageTracker
         if (DateTime.Now - recent.LastDamageTime <= TimeSpan.FromSeconds(5))
             recent.TotalDamage += hp;
         else
-        {
-            recent.TotalDamage  = hp;
-            recent.ArmorDamage  = armor;
-        }
+            recent.TotalDamage = hp;
 
-        recent.ArmorDamage    = armor;
-        recent.LastHitgroup   = hitgroup;
+        recent.LastHitgroup   = HitgroupToString(hitgroup);
         recent.LastDamageTime = DateTime.Now;
     }
 
@@ -114,15 +104,14 @@ public class DamageTracker
 
     public static string HitgroupToString(int hitgroup) => hitgroup switch
     {
-        0  => "Generic",
         1  => "Head",
         2  => "Chest",
         3  => "Stomach",
-        4  => "Left Arm",
-        5  => "Right Arm",
-        6  => "Left Leg",
-        7  => "Right Leg",
+        4  => "L.Arm",
+        5  => "R.Arm",
+        6  => "L.Leg",
+        7  => "R.Leg",
         10 => "Neck",
-        _  => "Unknown"
+        _  => ""
     };
 }
